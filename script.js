6b65ccf7cd7d38fbeb22a69ff0ab47eaf10d69b2
Kevin P. <127263761+desl8@users.noreply.github.com>
2024-10-04 14:21:07 -0700

    Calendar additions working
    thx Henrick

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-array = [];
-document.querySelector("#clearbutton").addEventListener("click", function(){array = []; console.log("Cleared calendar");});
-document.querySelector("#addbutton").addEventListener("click", function(){array = []; console.log("Added calendar item"); updateCalendar;});
+let array = [];
 function updateCalendar(){
-    
+    let schedule = document.getElementById("InnerSchedule");
+    Array.from(document.getElementsByClassName("RemovableRow")).forEach((e)=>{e.remove();console.log(e.innerText)});
+    for(let i = 0; i < array.length; i ++)
+    {
+        let row = document.createElement("tr");
+        schedule.appendChild(row);
+        let activityCell = document.createElement("td");   
+        activityCell.innerText = array[i][0];
+        let expectedTimeCell = document.createElement("td")
+        expectedTimeCell.innerText = array[i][1]
+        row.appendChild(activityCell);
+        row.appendChild(expectedTimeCell);
+        row.className = "RemovableRow"
+    }
 }
+document.querySelector("#clearbutton").addEventListener("click", function(){array = []; updateCalendar();});
+document.querySelector("#addbutton").addEventListener("click", function(){array.push([document.getElementById("namefield").value,document.getElementById("timefield").value]); updateCalendar();});
+doccument.querySelector("#")
+
+//Henrick absolutely cooked
+
+/*
+var canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d")
+canvas.width = 1000
+canvas.height = 1000;
+
+for(let i = 0; i < 330; i ++)
+{
+    for(let j = 0; j<330; j++)
+    {
+
+        setTimeout(()=>{
+            ctx.fillRect(i*3,j*3,3,3);
+            ctx.fillStyle = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+        },4*i+j*4);
+    }
+}
+document.body.appendChild(canvas);
+canvas.style.position = "absolute";
+canvas.style.zIndex = -1;
+canvas.style.left = 0;
+canvas.style.top = 0;
+
+setInterval(() => {
+    ctx.fillRect(Math.floor(Math.random()*1000),Math.floor(Math.random()*1000),3,3);
+    ctx.fillStyle = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+}, (0));
+*/
