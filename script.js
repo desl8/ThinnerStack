73c9177b1d6af71a959ca33ca34e2c41627ce65e
Kevin P. <127263761+desl8@users.noreply.github.com>
2024-10-04 13:31:21 -0700

    WIP Build
    Still not functioning, but basic UI is done

---
+++ script.js
@@ -0,0 +1,6 @@
+array = [];
+document.querySelector("#clearbutton").addEventListener("click", function(){array = []; console.log("Cleared calendar");});
+document.querySelector("#addbutton").addEventListener("click", function(){array = []; console.log("Added calendar item"); updateCalendar;});
+function updateCalendar(){
+    
+}
